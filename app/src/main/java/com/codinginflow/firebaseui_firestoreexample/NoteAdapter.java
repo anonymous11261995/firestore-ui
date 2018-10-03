ee62d8e3baa6f895bb9dd5c034b1869c5eccd7e5
TienTruong <tientruong@coloralpha.com>
2018-10-03 15:41:30 +0700

    swipe and click item

--- app/src/main/java/com/codinginflow/firebaseui_firestoreexample/NoteAdapter.java
+++ app/src/main/java/com/codinginflow/firebaseui_firestoreexample/NoteAdapter.java
@@ -9,32 +9,37 @@ import android.widget.TextView;
 
 import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
 import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.firebase.firestore.DocumentSnapshot;
 
- public class NoteAdapter extends FirestoreRecyclerAdapter<Note,NoteAdapter.NoteHolder> {
+public class NoteAdapter extends FirestoreRecyclerAdapter<Note, NoteAdapter.NoteHolder> {
+    private OnItemClickListener listener;
 
 
+    public NoteAdapter(@NonNull FirestoreRecyclerOptions<Note> options) {
+        super(options);
+    }
 
-     public NoteAdapter(@NonNull FirestoreRecyclerOptions<Note> options) {
-         super(options);
-     }
+    @Override
+    protected void onBindViewHolder(@NonNull NoteHolder holder, int position, @NonNull Note model) {
+        holder.textViewTitle.setText(model.getTitle());
+        holder.textViewDescription.setText(model.getDescription());
+        holder.textViewPriority.setText(String.valueOf(model.getPriority()));
 
-     @Override
-     protected void onBindViewHolder(@NonNull NoteHolder holder, int position, @NonNull Note model) {
-         holder.textViewTitle.setText(model.getTitle());
-         holder.textViewDescription.setText(model.getDescription());
-         holder.textViewPriority.setText(String.valueOf(model.getPriority()));
+    }
 
-     }
+    @NonNull
+    @Override
+    public NoteHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.note_item,
+                parent, false);
+        return new NoteHolder(v);
+    }
 
-     @NonNull
-     @Override
-     public NoteHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-         View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.note_item,
-         parent,false);
-         return new NoteHolder(v);
-     }
+    public void deleteItem(int position) {
+        getSnapshots().getSnapshot(position).getReference().delete();
+    }
 
-     class NoteHolder extends RecyclerView.ViewHolder {
+    class NoteHolder extends RecyclerView.ViewHolder {
         TextView textViewTitle;
         TextView textViewDescription;
         TextView textViewPriority;
@@ -44,7 +49,25 @@ import com.firebase.ui.firestore.FirestoreRecyclerOptions;
             textViewTitle = itemView.findViewById(R.id.text_view_title);
             textViewDescription = itemView.findViewById(R.id.text_view_description);
             textViewPriority = itemView.findViewById(R.id.text_view_priority);
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    int position = getAdapterPosition();
+                    if(position != RecyclerView.NO_POSITION && listener != null){
+                        listener.onItemClick(getSnapshots().getSnapshot(position),position);
+                    }
+                }
+            });
         }
+    }
+
+    public interface OnItemClickListener {
+        void onItemClick(DocumentSnapshot documentSnapshot, int position);
+    }
+
+    public void setOnClickListener(OnItemClickListener listener) {
+        this.listener = listener;
     }
 
 }
